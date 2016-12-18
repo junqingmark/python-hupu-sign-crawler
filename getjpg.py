23a2aa8ced0c610a2199da4101dc52a51db6fae9
junqingmark <junqingmark@163.com>
2016-12-18 17:59:40 +0800

    add set function

--- getjpg.py
+++ getjpg.py
@@ -1,25 +1,47 @@
 # -*- coding: utf-8 -*-
+
 import urllib
+
 import re
 
+
+
 def getHtml(url):
+
 	page = urllib.urlopen(url)
 	html = page.read()
 	return html
 
+
+
 def getSign(html):
+
 	reg = r'<div class="sign"><hr class="hr2" size="1" />(.+?)</div>'
+
 	#reg = r'uname="(.+?)"'
+
 	signre = re.compile(reg)
+
 	signlist = re.findall(signre, html)
-	for signurl in signlist:
-    	if signurl.find('qq') != -1:
-    		continue
+
+	signlistNocopy = set(signlist)
+
+	for signurl in signlistNocopy:
+
+		if signurl.find('QQ') != -1 or signurl.find(u'代购') != -1:
+			continue
 		else:
-    		print(signurl)
+			print(signurl)
+
+
 
 for i in range(2, 41):
+
 	html = getHtml("http://bbs.hupu.com/18039279-" + str(i) + ".html")
+
 	getSign(html.decode("gbk","ignore"))
 
+
+
 #print (html.decode("gbk","ignore"))
+
