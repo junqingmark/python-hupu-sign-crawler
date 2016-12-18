4a7c5a15a1075efbe82c6129cf72ab4ffb7ce6b0
junqingmark <junqingmark@163.com>
2016-12-18 23:15:27 +0800

    update crawl from bxj mainpage

--- getjpg.py
+++ getjpg.py
@@ -14,7 +14,7 @@ def getHtml(url):
 
 
 
-def getSign(html):
+def getSignInPage(html):
 
 	reg = r'<div class="sign"><hr class="hr2" size="1" />(.+?)</div>'
 
@@ -28,18 +28,30 @@ def getSign(html):
 
 	for signurl in signlistNocopy:
 
-		if signurl.find('QQ') != -1 or signurl.find(u'代购') != -1:
+		if signurl.find('QQ') != -1 or signurl.find(u'代购') != -1 or signurl.find(u'微信') != -1 or signurl.find(u'篮球') != -1:
 			continue
 		else:
 			print(signurl)
 
+def getLinkInPage(html):
+	reg = r'a id="" href="(.+?)"'
+	signre = re.compile(reg)
+	signlist = re.findall(signre, html)
+	for signurl in signlist:
+		HTML = getHtml("http://bbs.hupu.com" + signurl)
+		getSignInPage(HTML.decode("gbk","ignore"))
+		#print(signurl)
 
+for i in range(1, 11):
 
-for i in range(2, 41):
-
-	html = getHtml("http://bbs.hupu.com/18039279-" + str(i) + ".html")
-
-	getSign(html.decode("gbk","ignore"))
+	#html = getHtml("http://bbs.hupu.com/18039279-" + str(i) + ".html")
+	if i == 1:
+		html = getHtml("http://bbs.hupu.com/bxj")
+	else:
+		print("====This is page " + str(i) + "====")
+		html = getHtml("http://bbs.hupu.com/bxj-" + str(i))
+	
+	getLinkInPage(html)
 
 
 
